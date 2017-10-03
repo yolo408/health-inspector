a50e8bd031d8893ce81b03589abdcc9def9cb930
Goat <Kato@dhcp-v101-140.mobile.uci.edu>
2017-10-02 21:00:03 -0700

    Made hello world component

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App =  () => {
+
+
+
+    return (
+        <h1> Hello World!</h1>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
